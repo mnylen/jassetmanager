4f4da768c002319e36753d887b69c71f053667c8
Mikko Nylén <mikko.nylen@gmail.com>
2011-07-13 21:31:56 +0300

    Brand-new AssetBundle - build(...) to build the bundle

--- src/main/java/org/jassetmanager/AssetBundle.java
+++ src/main/java/org/jassetmanager/AssetBundle.java
@@ -2,59 +2,108 @@ package org.jassetmanager;
 
 import com.sun.istack.internal.NotNull;
 
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AssetBundle {
-    private final List<FilePattern> filePatterns;
     private byte[] content;
+    private boolean built;
+    private static final byte[] ASSET_SEPARATOR = new byte[] { '\r', '\n' };
 
     public AssetBundle() {
-        this.filePatterns = new ArrayList<FilePattern>();
         this.content = new byte[0];
+        this.built = false;
     }
 
-    public AssetBundle addFilePattern(FilePattern pattern) {
-        if (pattern == null) {
-            throw new IllegalArgumentException("pattern must not be null");
-        }
-        
-        this.filePatterns.add(pattern);
-        return this;
-    }
-
-    public AssetBundle addFilePattern(String pattern) {
-        if (pattern == null) {
-            throw new IllegalArgumentException("pattern must not be null");
-        }
-
-        this.filePatterns.add(new RegexFilePattern(pattern));
-        return this;
-    }
-
-    public boolean includesFile(@NotNull String fileName) {
-        for (FilePattern pattern : this.filePatterns) {
-            if (pattern.matches(fileName)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public AssetBundle addContent(byte[] content) {
-        byte[] newContent = new byte[this.content.length+content.length+2];
-        System.arraycopy(this.content, 0, newContent, 0, this.content.length);
-        System.arraycopy(content, 0, newContent, this.content.length, content.length);
-        newContent[newContent.length-2] = '\r';
-        newContent[newContent.length-1] = '\n';
-
-        this.content = newContent;
-        return this;
+    public boolean isBuilt() {
+        return this.built;
     }
 
     public byte[] getContent() {
-        return content;
+        return this.content;
+    }
+
+    public void build(@NotNull AssetBundleConfiguration config,
+                      @NotNull ServletContext context,
+                      @NotNull String userAgent,
+                      @NotNull List<String> allFilePaths) throws IOException {
+
+        this.content = new byte[0];
+        this.built = false;
+        
+        Map<Integer, List<String>> contentMap = new HashMap<Integer, List<String>>();
+        int maxPosition = 0;
+
+        for (String filePath : allFilePaths) {
+            int position = config.getContentPosition(filePath);
+            if (position == -1) {
+                continue;
+            } else if (position > maxPosition) {
+                maxPosition = position;
+            }
+
+            if (!(contentMap.containsKey(position))) {
+                contentMap.put(position, new ArrayList<String>());
+            }
+
+            contentMap.get(position).add(filePath);
+        }
+
+        for (int position = 0; position <= maxPosition; position++) {
+            if (!(contentMap.containsKey(position))) {
+                continue;
+            }
+            
+            List<String> filePaths = contentMap.get(position);
+            for (String filePath : filePaths) {
+                readAndAppendContent(context, filePath);
+            }
+        }
+
+        this.built = true;
+    }
+
+    private void readAndAppendContent(ServletContext context, String filePath) throws IOException {
+        byte[] buffer = new byte[1024];
+        InputStream is = null;
+
+        try {
+            is = context.getResourceAsStream(filePath);
+            if (is == null) {
+                throw new IOException("Could not open stream to asset '" + filePath + "'");
+            }
+
+            while (is.available() > 0) {
+                int length = is.read(buffer);
+
+                if (length != -1) {
+                    appendContent(buffer, length);
+                }
+            }
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (Exception e) {
+
+            }
+        }
+    }
+
+    private void appendContent(byte[] append, int appendLength) {
+        int newContentLength = this.content.length + ASSET_SEPARATOR.length + appendLength;
+
+        byte[] newContent = new byte[newContentLength];
+        System.arraycopy(this.content, 0, newContent, 0, this.content.length);
+        System.arraycopy(append, 0, newContent, this.content.length, appendLength);
+        System.arraycopy(ASSET_SEPARATOR, 0, newContent, (this.content.length + appendLength), ASSET_SEPARATOR.length);
+
+        this.content = newContent;
     }
 }
---
+++ src/test/java/org/jassetmanager/AssetBundleSimpleConcatenationTest.java
@@ -0,0 +1,54 @@
+package org.jassetmanager;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.servlet.ServletContext;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+
+public class AssetBundleSimpleConcatenationTest {
+    private ServletContext mockContext;
+    private List<String> allFilePaths;
+    private String userAgent;
+    private AssetBundle bundle;
+    private AssetBundleConfiguration config;
+    
+    @Before
+    public void setUp() {
+        this.mockContext = mock(ServletContext.class);
+        when(this.mockContext.getResourceAsStream("/css/reset.css")).thenReturn(
+                new ByteArrayInputStream("html, body { margin: 0; }".getBytes()));
+
+        when(this.mockContext.getResourceAsStream("/css/main.css")).thenReturn(
+                new ByteArrayInputStream("body { background-color: #000; }".getBytes()));
+
+        this.allFilePaths = new ArrayList<String>(Arrays.asList("/css/main.css", "/css/reset.css"));
+        this.userAgent = "";
+        this.bundle = new AssetBundle();
+        this.config = new AssetBundleConfiguration()
+                .addFilePattern(new RegexFilePattern("/css/reset.css"))
+                .addFilePattern(new RegexFilePattern("/css/main.css"));
+    }
+    
+    @Test
+    public void testBuildsConcatenation() throws IOException {
+        assertThat(this.bundle.isBuilt(), is(false));
+        assertThat(this.bundle.getContent(), equalTo(new byte[0]));
+        
+        this.bundle.build(config, mockContext, userAgent, allFilePaths);
+        
+        assertThat(this.bundle.isBuilt(), is(true));
+        assertThat(new String(this.bundle.getContent()), equalTo(
+                "html, body { margin: 0; }\r\nbody { background-color: #000; }\r\n"));
+    }
+}
