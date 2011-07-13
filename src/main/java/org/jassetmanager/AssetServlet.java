a7f427cb77fe8ec2ea53914a61fe2de24f422ed1
Mikko Nylén <mikko.nylen@gmail.com>
2011-07-13 23:06:06 +0300

    Initial AssetServlet

---
+++ src/main/java/org/jassetmanager/AssetServlet.java
@@ -0,0 +1,62 @@
+package org.jassetmanager;
+
+import com.sun.istack.internal.NotNull;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AssetServlet extends HttpServlet {
+    private final AssetRegistry registry;
+    private static final String USER_AGENT_HEADER = "User-Agent";
+    private static final String ASSET_ROOT_PATH = "/";
+
+    public AssetServlet() {
+        this.registry = new AssetRegistry();
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userAgent = request.getHeader(USER_AGENT_HEADER);
+        if (userAgent == null) {
+            userAgent = "";
+        }
+
+        String requestPath = request.getRequestURI();
+        AssetRegistry.RegistryEntry registryEntry = this.registry.get(requestPath, userAgent);
+        if (registryEntry == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        } else {
+            AssetBundle bundle = registryEntry.getBundle();
+            if (!(bundle.isBuilt())) {
+                bundle.build(findAllAssetPaths());
+            }
+
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType(registryEntry.getServeAsMimeType());
+            response.setContentLength(bundle.getContent().length);
+            response.getOutputStream().write(bundle.getContent());
+        }
+    }
+
+    protected List<String> findAllAssetPaths() {
+        final List<String> allAssetPaths = new ArrayList<String>();
+        AssetWalker.walkAssetTree(this.getServletContext(), "/", new AssetVisitor() {
+            public void visitAsset(@NotNull String path) {
+                allAssetPaths.add(path);
+            }
+        });
+
+        return allAssetPaths;
+    }
+
+    protected void configureBundle(String requestPath, String serveAsMimeType, UserAgentMatcher userAgentMatcher, AssetBundleConfiguration config) {
+        this.registry.register(requestPath, serveAsMimeType, userAgentMatcher,
+                new AssetBundle(config, this.getServletContext()));
+    }
+}
---
+++ src/test/java/org/jassetmanager/AssetServletTest.java
@@ -0,0 +1,42 @@
+package org.jassetmanager;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mortbay.jetty.testing.HttpTester;
+import org.mortbay.jetty.testing.ServletTester;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+public class AssetServletTest {
+    private ServletTester tester;
+
+
+    @Before
+    public void setUp() throws Exception {
+        this.tester = new ServletTester();
+        this.tester.setClassLoader(AssetServlet.class.getClassLoader());
+        this.tester.setContextPath("/");
+        this.tester.setResourceBase("src/test/resources");
+        this.tester.addServlet(TestAssetServlet.class, "*.css");
+        this.tester.start();
+    }
+
+    @Test
+    public void testBuildsAndOutputsBundleContent() throws Exception {
+        HttpTester request = new HttpTester();
+        request.setMethod("GET");
+        request.setHeader("Host", "tester");
+        request.setVersion("HTTP/1.1");
+        request.setURI("/css/application.css");
+
+        HttpTester response = new HttpTester();
+        response.parse(tester.getResponses(request.generate()));
+
+        assertThat(response.getContent(),
+                equalTo("html, body { margin: 0; }\r\nbody { background-color: #000; }\r\n"));
+    }
+}
