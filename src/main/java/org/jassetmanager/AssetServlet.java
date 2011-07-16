22791c9716e34f73ed93a619863c3676985c763c
Mikko Nylén <mikko.nylen@gmail.com>
2011-07-16 13:40:37 +0300

    Logging for the AssetServlet

--- src/main/java/org/jassetmanager/AssetServlet.java
+++ src/main/java/org/jassetmanager/AssetServlet.java
@@ -1,6 +1,7 @@
 package org.jassetmanager;
 
-import com.sun.istack.internal.NotNull;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AssetServlet extends HttpServlet {
+    private static final Log LOG = LogFactory.getLog(AssetServlet.class);
     private final AssetRegistry registry;
     private static final String USER_AGENT_HEADER = "User-Agent";
     private static final String IF_MODIFIED_SINCE_HEADER = "If-Modified-Since";
@@ -25,28 +27,33 @@ public class AssetServlet extends HttpServlet {
     }
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
         String userAgent = request.getHeader(USER_AGENT_HEADER);
         if (userAgent == null) {
             userAgent = "";
         }
 
-        String requestPath = request.getRequestURI();
-        AssetRegistry.RegistryEntry registryEntry = this.registry.get(requestPath, userAgent);
+        String uri = request.getRequestURI();
+        AssetRegistry.RegistryEntry registryEntry = this.registry.get(uri, userAgent);
         if (registryEntry == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         } else {
             AssetBundle bundle = registryEntry.getBundle();
             try {
-                rebuildBundleIfNeeded(bundle);
+                rebuildBundleIfNeeded(bundle, uri, userAgent);
             } catch (AssetException e) {
-                handleException(request, response, e);
+                handleException(uri, userAgent, response, e);
             }
 
             if (!(isBundleModified(bundle, request))) {
+                LOG.debug("Bundle for '" + uri + "' and User-Agent '" + userAgent + "' not modified.");
                 response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
             } else {
+                LOG.debug("Serving bundle for '" + uri + "' and User-Agent '" + userAgent + "'");
+                
                 response.setStatus(HttpServletResponse.SC_OK);
                 response.setContentType(registryEntry.getServeAsMimeType());
                 response.setContentLength(bundle.getContent().length);
@@ -55,21 +62,24 @@ public class AssetServlet extends HttpServlet {
         }
     }
 
-    protected void handleException(HttpServletRequest request,
+    protected void handleException(String uri,
+                                   String userAgent,
                                    HttpServletResponse response,
                                    AssetException e) throws IOException {
 
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-
         if (this.debug) {
             response.setContentType("text/plain");
             e.printStackTrace(response.getWriter());
         }
+
+        LOG.error("Failed to serve asset bundle for '" + uri + "' and UserAgent '" + userAgent + "'", e);
     }
 
-    protected void rebuildBundleIfNeeded(AssetBundle bundle) throws AssetException {
+    protected void rebuildBundleIfNeeded(AssetBundle bundle, String uri, String userAgent) throws AssetException {
         Assets assets = new Assets(this.getServletContext(), bundle.getConfiguration().getContextRootPath());
         if (bundle.getConfiguration().getBuildStrategy().isRebuildNeeded(bundle, assets)) {
+            LOG.info("Building bundle for '" + uri + "' and User-Agent '" + userAgent + "'");
             bundle.build(assets.listAssets());
         }
     }
@@ -83,7 +93,11 @@ public class AssetServlet extends HttpServlet {
         return true;
     }
 
-    protected void configureBundle(String requestPath, String serveAsMimeType, UserAgentMatcher userAgentMatcher, AssetBundleConfiguration config) {
+    protected void configureBundle(String requestPath,
+                                   String serveAsMimeType,
+                                   UserAgentMatcher userAgentMatcher,
+                                   AssetBundleConfiguration config) {
+        
         this.registry.register(requestPath, serveAsMimeType, userAgentMatcher,
                 new AssetBundle(config, this.getServletContext()));
     }
