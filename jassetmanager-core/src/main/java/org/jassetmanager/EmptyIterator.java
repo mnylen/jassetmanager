c1557b15ebec296181f28d5ad25f476d12ad89d1
mnylen <mikko.nylen@gmail.com>
2011-07-22 22:30:26 +0300

    Created parent pom and moved jassetmanager -> jassetmanager-core

---
+++ jassetmanager-core/src/main/java/org/jassetmanager/EmptyIterator.java
@@ -0,0 +1,19 @@
+package org.jassetmanager;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EmptyIterator<T> implements Iterator<T> {
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public T next() {
+        throw new NoSuchElementException();
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
