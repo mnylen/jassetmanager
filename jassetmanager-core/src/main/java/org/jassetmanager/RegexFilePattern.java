fef45026d1b3d94ad842918095f525c1278af1bc
mnylen <mikko.nylen@gmail.com>
2011-07-23 10:33:09 +0300

    Wrote JavaDoc for RegexFilePattern

--- jassetmanager-core/src/main/java/org/jassetmanager/RegexFilePattern.java
+++ jassetmanager-core/src/main/java/org/jassetmanager/RegexFilePattern.java
@@ -2,13 +2,32 @@ package org.jassetmanager;
 
 import java.util.regex.Pattern;
 
+/**
+ * Matches asset context paths against a regular expression.
+ *
+ * @author Mikko Nylén
+ */
 public class RegexFilePattern implements FilePattern {
     private final Pattern regexPattern;
 
+    /**
+     * Initializes a new <code>RegexFilePattern</code> that matches any assets against the given
+     * <code>Pattern</code>.
+     *
+     * <p>This is same as calling <code>new RegexFilePattern(Pattern.compile(pattern))</code></p>
+     *
+     * @param pattern the regex file pattern
+     */
     public RegexFilePattern(String pattern) {
         this.regexPattern = Pattern.compile(pattern);
     }
 
+    /**
+     * Initializes a new <code>RegexFilePattern</code> that matches any assets against
+     * the given <code>Pattern</code>.
+     *
+     * @param pattern the pattern to match against
+     */
     public RegexFilePattern(Pattern pattern) {
         this.regexPattern = pattern;
     }
