924fef0f4ec0537bee9e86c7837600f7ba83f6e0
mnylen <mikko.nylen@gmail.com>
2011-07-23 10:42:09 +0300

    Log the order of assets when building the bundle

--- jassetmanager-core/src/main/java/org/jassetmanager/BundleBuilder.java
+++ jassetmanager-core/src/main/java/org/jassetmanager/BundleBuilder.java
@@ -1,10 +1,14 @@
 package org.jassetmanager;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
 public class BundleBuilder {
+    private static final Log LOG = LogFactory.getLog(BundleBuilder.class);
     private static final byte[] ASSET_SEPARATOR = new byte[] { '\r', '\n' };
     private final FileSystem fs;
     private final BundleConfiguration configuration;
@@ -30,9 +34,13 @@ public class BundleBuilder {
     private void doBuild(Bundle bundle, BundleAssets assets) throws IOException, AssetException {
         ByteArrayOutputStream contentStream = new ByteArrayOutputStream();
         Iterator<Asset> it = assets.getAssets();
+
         while (it.hasNext()) {
             Asset asset = it.next();
+            LOG.info("Concatenating '" + asset.getContextPath() + "' to bundle.");
+            
             for (Manipulator manipulator : this.configuration.getPreManipulators()) {
+                LOG.debug("\tPre manipulating '" + asset.getContextPath() + "' with '" + manipulator.toString() + "'");
                 manipulator.preManipulate(asset, it.hasNext());
             }
 
@@ -43,6 +51,7 @@ public class BundleBuilder {
         bundle.update(assets, contentStream.toByteArray());
 
         for (Manipulator manipulator : this.configuration.getPostManipulators()) {
+            LOG.debug("Post manipulating bundle with '" + manipulator.toString());
             manipulator.postManipulate(bundle);
         }
     }
